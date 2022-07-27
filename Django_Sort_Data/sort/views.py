2346059cb4c5f5356e45aa360eb60a75d7f48ad2
Kartik Parmar <kartik.infynno@gmail.com>
2022-07-27 16:20:40 +0530

    Django Sort Data Updated Added Search Functionality Using Django Insted Of JS

--- Django_Sort_Data/sort/views.py
+++ Django_Sort_Data/sort/views.py
@@ -1,10 +1,11 @@
-from gettext import find
 from django.shortcuts import redirect, render
 from .forms import Sort_form
 from .models import Sort
 from datetime import datetime, date
 from django.forms.models import model_to_dict
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
+from django.db.models import Q
+
 
 
 def find_age(born):
@@ -15,23 +16,46 @@ def find_age(born):
 
     return age
 
-
 def index(request):
-    data1 = Sort.objects.all()
-    data = []
-    for x in data1:
-        data.append(model_to_dict(x, fields=None, exclude=None))
-    for i in data:
-        age = find_age(str(i['dob']))
-        i['age'] = age
-    p = Paginator(data, 5)
-    page_number = request.GET.get('page')
-    try:
-        page_obj = p.get_page(page_number)
-    except PageNotAnInteger:
-        page_obj = p.page(1)
-    except EmptyPage:
-        page_obj = p.page(p.num_pages)
+    filter_data_list = []
+    data_list = []
+    if 'q' in request.GET:
+        q = request.GET['q']
+        # data = Data.objects.filter(last_name__icontains=q)
+        multiple_q = Q(Q(name__icontains=q) | Q(email__icontains=q))
+        data = Sort.objects.filter(multiple_q)
+        for x in data:
+
+            filter_data_list.append(model_to_dict(x,fields=None,exclude=None,))
+        for i in filter_data_list:
+            age = find_age(str(i['dob']))
+            i['age'] = age
+        p = Paginator(filter_data_list, 5)
+        page_number = request.GET.get('page')
+        try:
+            page_obj = p.get_page(page_number)
+        except PageNotAnInteger:
+            page_obj = p.page(1)
+        except EmptyPage:
+            page_obj = p.page(p.num_pages)
+    else:
+        data = Sort.objects.all()
+        for x in data:
+            data_list.append(model_to_dict(x, fields=None, exclude=None))
+
+        for i in data_list:
+            age = find_age(str(i['dob']))
+            i['age'] = age
+
+        p = Paginator(data_list,5)
+        page_number = request.GET.get('page')
+        try:
+            page_obj = p.get_page(page_number)
+        except PageNotAnInteger:
+            page_obj = p.page(1)
+        except EmptyPage:
+            page_obj = p.page(p.num_pages)
+
     context = {
         'data': page_obj
     }
